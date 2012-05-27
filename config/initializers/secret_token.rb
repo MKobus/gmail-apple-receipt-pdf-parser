4ce447c510448a20129b9606d3eacad3440372b5
Carlos Gabaldon <cgabaldon@gmail.com>
2012-05-27 07:01:07 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FetchIt::Application.config.secret_token = '9127c91b5dcebbc60c945149b7b59b4943214b1303e1a3ab1476bfede92a19d9d4b84074e893783ffcaaec385212098ecf8176b0dc2f55dc247f31d7e126b50c'
